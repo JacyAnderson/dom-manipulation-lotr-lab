b2b2cabaace0ef2fbcffb343f0d3c81c6e7fc2f8
JacyAnderson <jacyjamesanderson@gmail.com>
2017-03-17 09:03:57 -0600

    add parts 1 2 3 4 5 6

--- starter-code/app/scripts/fellowship.js
+++ starter-code/app/scripts/fellowship.js
@@ -24,31 +24,65 @@ var body = document.querySelector('body');
 
 
 function makeMiddleEarth() {
-  // create a section tag with an id of middle-earth
-  // inside, add each land as an article tag
-  // inside each article tag include an h1 with the name of the land
-  // append middle-earth to your document body
+    // create a section tag with an id of middle-earth
+    var middleEarthSection = document.createElement('section');
+       // inside, add each land as an article tag
+      for (var i = 0; i < lands.length; i++) {
+        var eachLand = document.createElement('article');
+        middleEarthSection.appendChild(eachLand);
+        console.log(middleEarthSection);
+        // inside each article tag include an h1 with the name of the land
+        var h1 = document.createElement('h1');
+        h1.textContent = lands[i];
+        eachLand.appendChild(h1);
+        console.log(eachLand);
+      }
+      // append middle-earth to your document body
+  body.appendChild(middleEarthSection);  
 }
 
 makeMiddleEarth();
 
 
 // Part 2
-
+var theShire = body.getElementsByTagName('article')[0];
+var rivendell = body.getElementsByTagName('article')[1];
+var mordor = body.getElementsByTagName('article')[2];
 function makeHobbits() {
+
+  var hobbitList = document.createElement('ul');
+    console.log(hobbitList);
+    for (var i =0; i < hobbits.length; i++){
+      var hobbit =document.createElement('li');
+      hobbit.className = 'hobbit';
+      hobbit.textContent = hobbits[i];
+      hobbitList.appendChild(hobbit);
+      console.log(hobbitList);
+    }
+    theShire.appendChild(hobbitList);
   // display an unordered list of hobbits in the shire (which is the first article tag on the page)
   // give each hobbit a class of hobbit
-}
 
+}
+makeHobbits();
 
 // Part 3
-
+var frodo = body.getElementsByTagName('li')[0];
 function keepItSecretKeepItSafe() {
-  // create a div with an id of 'the-ring'
+   // create a div with an id of 'the-ring'
+  var ring = document.createElement('div');
+
+  ring.id = 'the-ring';
   // give the div a class of 'magic-imbued-jewelry'
+  ring.className = 'magic-imbued-jewelry';
+  console.log(ring);
   // add an event listener so that when a user clicks on the ring, the nazgulScreech function (provided) is invoked
+  ring.addEventListener("click",nazgulScreech);
   // add the ring as a child of Frodo
+  frodo.appendChild(ring);
 }
+
+keepItSecretKeepItSafe();
 
 
 // Part 4
@@ -56,35 +90,51 @@ function keepItSecretKeepItSafe() {
 
 function makeBuddies() {
   // create an aside tag
+  var aside = document.createElement('aside');
+    console.log(aside);
   // attach an unordered list of the 'buddies' in the aside
+  var buddyList = document.createElement('ul');
+  for (var i = 0; i < buddies.length; i++) {
+    var buddy = document.createElement('li');
+    buddy.textContent = buddies[i];
+    buddyList.appendChild(buddy);
+    aside.appendChild(buddyList)
+    console.log(aside);
+  }
   // insert your aside as a child element of rivendell
+  rivendell.appendChild(aside);
 }
 
-
+makeBuddies();
 // Part 5
 
-
+var strider = rivendell.getElementsByTagName('li')[3];
 function beautifulStranger() {
+  strider.textContent = 'Aragorn';
   // change the 'Strider' textnode to 'Aragorn'
 }
-
+beautifulStranger();
 
 // Part 6
-
+var hobbits = theShire.getElementsByTagName('ul');
 function leaveTheShire() {
   // assemble the hobbits and move them to Rivendell
+  rivendell.appendChild(hobbits[0]);
 }
 
-
+leaveTheShire();
 // Part 7
 
-
+var membersOfFellowship = rivendell.querySelector('li');
 function forgeTheFellowShip() {
   // create a new div called 'the-fellowship' within rivendell
+  var the-fellowship = document.createElement('div');
+  //rivendell.appendChild(the-fellowship);
+  //console.log(the-fellowship);
   // add each hobbit and buddy one at a time to 'the-fellowship'
   // after each character is added make an alert that they have joined your party
 }
-
+forgeTheFellowShip();
 
 // Part 8
 
